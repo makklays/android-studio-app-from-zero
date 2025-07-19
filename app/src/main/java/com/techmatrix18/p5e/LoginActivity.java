e167f8fa768873e5a392e6f60a9bfd092819a962
Alexander <alexander@makklays.com.ua>
2025-07-19 20:56:10 +0200

    19-07-2025 Add new check token for page 'Login'.

--- app/src/main/java/com/techmatrix18/p5e/LoginActivity.java
+++ app/src/main/java/com/techmatrix18/p5e/LoginActivity.java
@@ -2,12 +2,16 @@ package com.techmatrix18.p5e;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,6 +26,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Проверяем есть ли token
+        SharedPreferences prefs = getSharedPreferences("Auth", MODE_PRIVATE);
+        AtomicReference<String> token = new AtomicReference<>(prefs.getString("token", null)); // null — значение по умолчанию
+        if (token.get() != null) {
+            // Токен успешно получен
+            Log.d("TOKEN", "Token: " + token);
+
+            // Переход на главную активити (например, MainActivity)
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
@@ -40,6 +57,11 @@ public class LoginActivity extends AppCompatActivity {
 
             if (username.equals(correctUsername) && password.equals(correctPassword)) {
                 Toast.makeText(LoginActivity.this, "Успешный вход", Toast.LENGTH_SHORT).show();
+
+                // Добавляем token
+                token.set("1111-2222-3333-4444");
+                //SharedPreferences prefs = getSharedPreferences("Auth", MODE_PRIVATE);
+                prefs.edit().putString("token", token.get()).apply();
 
                 // Переход на главную активити (например, MainActivity)
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
